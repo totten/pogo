2476906352b6e0f9ab3bbf1fd71d383150634870
Tim Otten <totten@civicrm.org>
2019-11-30 01:51:34 -0800

    Update help text

--- src/Command/HelpCommand.php
+++ src/Command/HelpCommand.php
@@ -8,17 +8,21 @@ class HelpCommand {
   public function run(PogoInput $input) {
     $cmd = basename($input->program);
     echo "Example: Run the given script\n";
-    echo "  $cmd run [build-options] <script-file> [--] [script-options]\n";
+    echo "  $cmd run [download-options] <script-file> [--] [script-options]\n";
     echo "\n";
-    echo "Example: Create a folder with the script and all dependencies\n";
-    echo "  $cmd create [build-options] <script-file>\n";
+    echo "Example: Download dependencies to the given folder\n";
+    echo "  $cmd dl [download-options] <script-file>\n";
     echo "\n";
     //    echo "Example: Update dependencies in an existing project directory\n";
-    //    echo "  $cmd update <script-file>\n";
+    //    echo "  $cmd update\n";
     //    echo "\n";
-    echo "Build Options:\n";
+    //    echo "Example: Remove any expired builds from the base folder\n";
+    //    echo "  $cmd clean\n";
+    //    echo "\n";
+
+    echo "Download Options:\n";
     echo "  -f        Force; recreate project, even if it appears current\n";
-    echo "  -o <out>  Output directory\n";
+    echo "  -o=<out>  Output directory\n";
     echo "\n";
     echo "\n";
     echo "Environment:\n";
