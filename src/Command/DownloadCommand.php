554341a33ae3a92db269ea80dce446d3139d9927
Tim Otten <totten@civicrm.org>
2019-11-30 01:51:22 -0800

    Add 'dl' command

---
+++ src/Command/DownloadCommand.php
@@ -0,0 +1,39 @@
+<?php
+namespace Pogo\Command;
+
+use Pogo\PogoProject;
+use Pogo\PogoInput;
+use Pogo\Runner\EvalRunner;
+use Pogo\Runner\FileRunner;
+use Pogo\Runner\DashBRunner;
+use Pogo\Runner\DataRunner;
+use Pogo\Runner\IncludeRunner;
+
+class DownloadCommand {
+
+  use DownloadCommandTrait;
+
+  public function run(PogoInput $input) {
+    if (empty($input->arguments)) {
+      throw new \Exception("[pogo dl] Missing required file name");
+    }
+
+    if (count($input->arguments) > 1 && $input->getOption(['out', 'o'])) {
+      throw new \Exception("[pogo dl] The --out=<path> can only be used with one script at a time.");
+    }
+
+    // TODO: realpath($target) but using getenv(PWD) or `pwd` to preserve symlink structure
+
+    foreach ($input->arguments as $target) {
+      if (!file_exists($target)) {
+        throw new \Exception("[pogo dl] Non-existent file: $target");
+      }
+    }
+
+    foreach ($input->arguments as $target) {
+      $this->initProject($input, $target);
+    }
+    return 0;
+  }
+
+}
