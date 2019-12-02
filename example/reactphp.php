54cec7f435cf59a064e95afaf9c2615b6b95a64f
Tim Otten <totten@civicrm.org>
2019-12-02 03:00:01 -0800

    example/reactphp - Show how to run an httpd

---
+++ example/reactphp.php
@@ -0,0 +1,23 @@
+#!/usr/bin/env pogo
+<?php
+#!require react/http: ^0.8.5
+
+use Psr\Http\Message\ServerRequestInterface;
+use React\EventLoop\Factory;
+use React\Http\Response;
+use React\Http\Server;
+
+$loop = Factory::create();
+$server = new Server(function (ServerRequestInterface $request) {
+    return new Response(
+        200,
+        array(
+            'Content-Type' => 'text/plain'
+        ),
+        "Hello world\n"
+    );
+});
+$socket = new \React\Socket\Server(isset($argv[1]) ? $argv[1] : '0.0.0.0:0', $loop);
+$server->listen($socket);
+echo 'Listening on ' . str_replace('tcp:', 'http:', $socket->getAddress()) . PHP_EOL;
+$loop->run();
