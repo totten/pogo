06b03459d53314643f7eba7ed037d672dc096f9e
Tim Otten <totten@civicrm.org>
2019-12-05 14:39:50 -0800

    Update to current "box"; enable php scope compactor; add build script

---
+++ build.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+## Determine the absolute path of the directory with the file
+## usage: absdirname <file-path>
+function absdirname() {
+  pushd $(dirname $0) >> /dev/null
+    pwd
+  popd >> /dev/null
+}
+
+PRJDIR=$(absdirname "$0")
+export PATH="$PRJDIR/bin:$PATH"
+
+set -ex
+composer install --prefer-dist --no-progress --no-suggest --no-dev
+which box
+php -d phar.read_only=0 `which box` build -v
