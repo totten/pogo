001bd79118fd097dee6e7038a43be1f3b694cea6
Tim Otten <totten@civicrm.org>
2022-11-01 17:40:38 -0700

    scoper.inc.php - Fix symonfy's php polyfills

--- scoper.inc.php
+++ scoper.inc.php
@@ -1,6 +1,12 @@
 <?php declare(strict_types = 1);
 
+$polyfills = \array_merge(
+  (array) glob(__DIR__ . '/vendor/symfony/polyfill-*/*.php'),
+  (array) glob(__DIR__ . '/vendor/symfony/polyfill-*/Resources/stubs/*.php')
+);
+
 return [
   'prefix' => 'PogoPhar',
-  'exclude-files' => ['templates/pogolib.php'],
+  'exclude-files' => array_merge(['templates/pogolib.php'], $polyfills),
+  'exclude-namespaces' => ['Symfony\Polyfill'],
 ];
